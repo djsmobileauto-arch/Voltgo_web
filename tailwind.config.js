312e6aeb86755572823a9da446566a15e514592b
juan Fernandez <djsmobileauto@gmail.com>
2025-08-24 02:27:40 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "volt-blue": "var(--volt-blue)",
+        "volt-green": "var(--volt-green)",
+      },
+    },
   },
   plugins: [],
-}
+};

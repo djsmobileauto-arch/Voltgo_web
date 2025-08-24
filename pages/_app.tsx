2db01ee09bdfe55c1aeccba7f7e3a03b2688ac12
juan Fernandez <djsmobileauto@gmail.com>
2025-08-24 13:25:54 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import type { AppProps } from "next/app";
-// Si tu alias "@" está configurado en tsconfig.json, usa esta línea:
-import Navbar from "../components/Navbar";
-
-// Si NO tienes alias "@", usa en cambio:
-// import Navbar from "../components/Navbar";
-import "@/styles/globals.css";
+import Navbar from "../components/Navbar";   // ruta relativa
+import "../styles/globals.css";              // ruta relativa
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Navbar />
-      {/* padding-top para que el contenido no quede debajo del navbar fijo */}
+      {/* espacio para que el navbar fijo no tape el contenido */}
       <div className="pt-16">
         <Component {...pageProps} />
       </div>

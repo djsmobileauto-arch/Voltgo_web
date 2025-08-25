0b3fa9beb22a0291f79065d3c1eb3072b80177e1
juan Fernandez <djsmobileauto@gmail.com>
2025-08-25 13:51:59 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
+// pages/_app.tsx
 import type { AppProps } from "next/app";
-import Navbar from "../components/Navbar";   // ruta relativa
-import "../styles/globals.css";              // ruta relativa
+import NavBar from "../components/Navbar";
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navbar />
-      {/* espacio para que el navbar fijo no tape el contenido */}
+      <NavBar />
       <div className="pt-16">
         <Component {...pageProps} />
       </div>

ab68cb40497cf85b3185ec4856c0b0fc1e47423b
juan Fernandez <djsmobileauto@gmail.com>
2025-08-25 14:47:36 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-// pages/_app.tsx
 import type { AppProps } from "next/app";
 import NavBar from "../components/Navbar";
 import "../styles/globals.css";
@@ -7,6 +6,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <NavBar />
+      {/* Deja espacio bajo el header fijo */}
       <div className="pt-16">
         <Component {...pageProps} />
       </div>

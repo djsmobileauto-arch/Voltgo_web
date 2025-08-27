5f1af17097f596d39f7aea469b620a0de789cf28
juan Fernandez <djsmobileauto@gmail.com>
2025-08-27 19:08:47 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,26 @@
-import '../styles/globals.css'
+// pages/_app.tsx
 import type { AppProps } from 'next/app'
-import Footer from '../components/Footer'
+import Head from 'next/head'
+import '../styles/globals.css'
+import Footer from '../components/Footer' // <- OJO: F mayúscula
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-screen flex flex-col">
-      {/* Contenido principal */}
-      <main className="flex-1">
-        <Component {...pageProps} />
-      </main>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>VoltGo</title>
+      </Head>
 
-      {/* Footer en TODAS las páginas */}
-      <Footer />
-    </div>
+      <div className="min-h-screen flex flex-col">
+        {/* Contenido principal */}
+        <main className="flex-1">
+          <Component {...pageProps} />
+        </main>
+
+        {/* Footer en TODAS las páginas */}
+        <Footer />
+      </div>
+    </>
   )
 }

fdb71a3301f497c79c3db31b9cd4cc75c4f3b4d8
juan Fernandez <djsmobileauto@gmail.com>
2025-09-07 23:10:04 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="flex items-center justify-between px-6 py-4 bg-blue-600">
-      <div className="flex items-center space-x-3">
+      {/* Logo más grande y con presencia */}
+      <div className="flex items-center">
         <Image
           src="/logo-voltgo.png"
           alt="VoltGo Logo"
-          width={160}
-          height={50}
+          width={200}   // puedes subirlo a 220 o 240 si quieres aún más grande
+          height={60}
           priority
         />
       </div>

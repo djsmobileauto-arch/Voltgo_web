0344e24409582d60fa877a029067586ddb77d108
juan Fernandez <djsmobileauto@gmail.com>
2025-09-12 15:55:28 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="flex items-center justify-between px-6">
-      {/* Logo */}
-      <div className="flex items-center border border-red-500 bg-yellow-200">
+      {/* American Flag */}
+      <div className="flex items-center">
         <Image
-          src="/voltgo-logo-final.png"
-          alt="VoltGo Logo"
-          width={220}
-          height={70}
+          src="/us-flag.png"
+          alt="American Flag"
+          width={100}
+          height={60}
           priority
         />
       </div>
@@ -19,6 +19,10 @@ export default function Header() {
         <a href="/">Home</a>
         <a href="/services">Services</a>
         <a href="/roadside">Roadside</a>
+        <a href="/app">App</a>
+        <a href="/subscribe">Subscribe</a>
+        <a href="/contact">Contact</a>
+        <a href="/pricing">Pricing</a>
       </nav>
     </header>
   );

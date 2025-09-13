32b94a19b0590b08352dc4823078a419343379fa
juan Fernandez <djsmobileauto@gmail.com>
2025-09-12 22:03:51 -0400

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,42 +4,28 @@
 import Link from "next/link";
 
 export default function Navbar() {
-return (
-<>
-{/* Nav menu */}
-<nav className="hidden md:flex space-x-8">
-<Link href="/" className="text-white hover:text-gray-200">
-Home
-</Link>
-<Link href="/services" className="text-white hover:text-gray-200">
-Services
-</Link>
-<Link href="/roadside" className="text-white hover:text-gray-200">
-Roadside
-</Link>
-<Link href="/app" className="text-white hover:text-gray-200">
-App
-</Link>
-<Link href="/subscribe" className="text-white hover:text-gray-200">
-Subscribe
-</Link>
-<Link href="/contact" className="text-white hover:text-gray-200">
-Contact
-</Link>
-<Link href="/pricing" className="text-white hover:text-gray-200">
-Pricing
-</Link>
-</nav>
+  return (
+    <div className="flex items-center justify-between w-full">
+      {/* Links centrados */}
+      <nav className="flex flex-1 justify-center space-x-8">
+        <Link href="/" className="text-white hover:text-gray-200">Home</Link>
+        <Link href="/services" className="text-white hover:text-gray-200">Services</Link>
+        <Link href="/roadside" className="text-white hover:text-gray-200">Roadside</Link>
+        <Link href="/app" className="text-white hover:text-gray-200">App</Link>
+        <Link href="/subscribe" className="text-white hover:text-gray-200">Subscribe</Link>
+        <Link href="/contact" className="text-white hover:text-gray-200">Contact</Link>
+        <Link href="/pricing" className="text-white hover:text-gray-200">Pricing</Link>
+      </nav>
 
-{/* Emergency button */}
-<div className="flex-shrink-0">
-<a
-href="tel:18334865846"
-className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
->
-Emergency 24/7 · Call 1-833-4 VOLTGO
-</a>
-</div>
-</>
-);
+      {/* Botón verde derecha */}
+      <div className="flex-shrink-0 ml-4">
+        <a
+          href="tel:18334865846"
+          className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+        >
+          1-833-4VOLTGO
+        </a>
+      </div>
+    </div>
+  );
 }

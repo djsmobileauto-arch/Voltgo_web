4e7622073be788f392a7438dbfe3a092e36a8b9f
juan Fernandez <djsmobileauto@gmail.com>
2025-09-12 21:15:52 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,10 +10,10 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
 
-          {/* Bandera americana */}
-          <div>
+          {/* Bandera izquierda */}
+          <div className="flex items-center">
             <Image
-              src="/usa-flag.png"   // debe estar en /public/usa-flag.png
+              src="/usa-flag.png"   // archivo en /public/usa-flag.png
               alt="USA Flag"
               width={36}
               height={24}
@@ -21,11 +21,13 @@ export default function Header() {
             />
           </div>
 
-          {/* Menú de navegación */}
-          <Navbar />
+          {/* Menú centro */}
+          <div className="flex-1 flex justify-center">
+            <Navbar />
+          </div>
 
-          {/* Botón verde con teléfono */}
-          <div>
+          {/* Botón verde derecha */}
+          <div className="flex items-center">
             <a
               href="tel:18334VOLTGO"
               className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow"

85d3da3e5b214056f82109133fdb016f0393e98f
juan Fernandez <djsmobileauto@gmail.com>
2025-09-12 23:09:45 -0400

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,8 +14,8 @@ export default function Navbar() {
   <img
     src="/us-flag.png"
     alt="USA Flag"
-    width={60}     // ancho mayor
-    height={40}    // alto proporcional
+    width={100}     // ancho mayor
+    height={60}    // alto proporcional
     className="rounded-sm shadow"
   />
 </div>

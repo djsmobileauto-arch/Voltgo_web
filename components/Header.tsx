663576291c4355e1cae5bf219f05a82f655c7355
juan Fernandez <djsmobileauto@gmail.com>
2025-09-12 22:19:45 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,14 +6,14 @@ import Navbar from "./Navbar";
 
 export default function Header() {
   return (
-    <header className="bg-blue-700 shadow-md">
+    <header className="bg-blue-700 shadow-md fixed top-0 w-full z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
 
           {/* Bandera izquierda */}
           <div className="flex items-center">
             <Image
-              src="/usa-flag.png"   // archivo en /public/usa-flag.png
+              src="/usa-flag.png"
               alt="USA Flag"
               width={36}
               height={24}
@@ -21,20 +21,8 @@ export default function Header() {
             />
           </div>
 
-          {/* Menú centro */}
-          <div className="flex-1 flex justify-center">
-            <Navbar />
-          </div>
-
-          {/* Botón verde derecha */}
-          <div className="flex items-center">
-            <a
-              href="tel:18334VOLTGO"
-              className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow"
-            >
-              1-833-4VOLTGO
-            </a>
-          </div>
+          {/* Navbar centrado con botón integrado */}
+          <Navbar />
 
         </div>
       </div>

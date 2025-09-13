d6fdc8014516216bf27396afe380316bc2fa8c0a
juan Fernandez <djsmobileauto@gmail.com>
2025-09-12 20:46:54 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,23 @@
-// components/Header.tsx
+// components/Navbar.tsx
 "use client";
 
 import Link from "next/link";
-import Navbar from "./Navbar"; // Importa el Navbar
 
-export default function Header() {
-return (
-<header className="bg-blue-700">
-<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-<div className="flex items-center justify-between h-16">
-
-{/* Logo */}
-<div className="flex-shrink-0">
-<Link href="/">
-<img
-className="h-10 w-auto"
-src="/voltgo-logo-final.png"
-alt="VoltGo Logo"
-/>
-</Link>
-</div>
-
-{/* Usa el componente Navbar aquí */}
-<Navbar />
-
-</div>
-</div>
-</header>
-);
+export default function Navbar() {
+  return (
+    <nav className="flex items-center gap-6">
+      <Link href="/" className="hover:text-blue-700">
+        Home
+      </Link>
+      <Link href="/about" className="hover:text-blue-700">
+        About
+      </Link>
+      <Link href="/services" className="hover:text-blue-700">
+        Services
+      </Link>
+      <Link href="/contact" className="hover:text-blue-700">
+        Contact
+      </Link>
+    </nav>
+  );
 }
